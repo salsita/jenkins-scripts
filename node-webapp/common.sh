3b543e22a37cb32351fd7c7b6ef5a0e77b01a5a9
Tomas Brambora <tomas@salsitasoft.com>
2013-10-10 17:01:16 +0200

    Fixed port.

--- node-webapp/common.sh
+++ node-webapp/common.sh
@@ -4,7 +4,7 @@ set -x
 
 # This file should be idempotent. It's sourced from various other scripts.
 
-DOCKER="docker -H tcp://127.0.0.1:4252"
+DOCKER="docker -H tcp://127.0.0.1:4245"
 
 IMAGE_TAG="${service}-${environment}"
 IMAGE_TAG_BUILD="${service}-${environment}-current-build"
