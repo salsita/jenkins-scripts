bdb15cf7ea677b6df3bacbc0b547a02f37870357
Tomas Brambora <tomas.brambora@gmail.com>
2013-12-02 18:48:00 +0100

    Update unit-test.sh

--- node-webapp/unit-test.sh
+++ node-webapp/unit-test.sh
@@ -7,7 +7,7 @@ source common.sh
 
 cd ${WORKSPACE}
 
-$DOCKER run -cidfile ${CID_DIR}/test-${BUILD_NUMBER}.cid -v '${WORKSPACE}/srv/:/srv/project/' -e NODE_ENV=${environment} ${docker_opts} ${IMAGE_TAG_BUILD} /bin/bash -c "cd /srv/project/deploy && make test"
+$DOCKER run -cidfile ${CID_DIR}/test-${BUILD_NUMBER}.cid -v '${WORKSPACE}/:/srv/project/' -e NODE_ENV=${environment} ${docker_opts} ${IMAGE_TAG_BUILD} /bin/bash -c "cd /srv/project/deploy && make test"
 
 sudo chown -R jenkins-slave:jenkins-slave ${WORKSPACE}
 
