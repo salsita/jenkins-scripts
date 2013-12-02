aca6cc3af1c57f134eb217008b9261325b751600
Tomas Brambora <tomas.brambora@gmail.com>
2013-12-02 18:32:57 +0100

    Update unit-test.sh

--- node-webapp/unit-test.sh
+++ node-webapp/unit-test.sh
@@ -7,25 +7,11 @@ source common.sh
 
 cd ${WORKSPACE}
 
-$DOCKER run -cidfile ${CID_DIR}/test-${BUILD_NUMBER}.cid -e NODE_ENV=${environment} ${docker_opts} ${IMAGE_TAG_BUILD} /bin/bash -c "cd /srv/project/deploy && make test"
+$DOCKER run -cidfile ${CID_DIR}/test-${BUILD_NUMBER}.cid -v '${WORKSPACE}/srv/:/srv/project/' -e NODE_ENV=${environment} ${docker_opts} ${IMAGE_TAG_BUILD} /bin/bash -c "cd /srv/project/deploy && make test"
+
+sudo chown -R jenkins-slave:jenkins-slave ${WORKSPACE}
 
 CONTAINER=`cat ${CID_DIR}/test-${BUILD_NUMBER}.cid`
-
-IMAGE=$($DOCKER commit ${CONTAINER})
-set +e
-TEST_RESULTS=$($DOCKER run -cidfile ${CID_DIR}/find-test-${BUILD_NUMBER}.cid ${docker_opts} ${IMAGE} /bin/bash -c "find /srv/project/ -name 'test-results*.xml' | grep -v node_modules")
-set -e
-$DOCKER rmi ${IMAGE}
-
-# Delete the old test results (just in case we're not deleting the workspace).
-# If we didn't do that, the old test results would be copied into the Dicker container
-# and Jenkins would complain it's an old result. Ugh.
-rm -f test-results*.xml
-
-# Copy the test results to the host machine.
-for TEST_FILE in ${TEST_RESULTS}; do
-  $DOCKER cp ${CONTAINER}:${TEST_FILE} ${WORKSPACE}/
-done
 
 # Exit with the same value that the process exited with.
 RC=$($DOCKER wait ${CONTAINER})
