98a6e26711d96260f3d29232fc448eae979cc71b
Tomas Brambora <tomas.brambora@gmail.com>
2013-12-02 19:14:59 +0100

    Update cleanup.sh

--- node-webapp/cleanup.sh
+++ node-webapp/cleanup.sh
@@ -7,16 +7,5 @@ source common.sh
 
 sudo chown -R jenkins-slave:jenkins-slave ${WORKSPACE}
 
-if [ `$DOCKER images | grep ${IMAGE_TAG}` -eq 0 ]; then
-  # Remove the build image.
-  $DOCKER rmi ${IMAGE_TAG_BUILD}
-fi
-
-# Remove the containers to free disk spaaace.
-for cidfile in `ls ${CID_DIR}`; do
-  $DOCKER rm `cat ${CID_DIR}/${cidfile}`
-  rm -f ${CID_DIR}/${cidfile}
-done
-
 # Remove artifacts
 rm -rf ${ARTIFACTS_DIR}
